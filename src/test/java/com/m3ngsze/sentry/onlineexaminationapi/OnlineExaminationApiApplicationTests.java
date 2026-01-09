0e96d488243739b4c2d968a8ceb60d9a00338079
M3NGSZE <mengse.thun13@gmail.com>
2026-01-09 15:55:07 +0700

    first commit

---
+++ src/test/java/com/m3ngsze/sentry/onlineexaminationapi/OnlineExaminationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m3ngsze.sentry.onlineexaminationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExaminationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
