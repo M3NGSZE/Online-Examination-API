3016c810e57940d579eac762e06d9970b858ec03
M3NGSZE <mengse.thun13@gmail.com>
2026-05-31 15:02:38 +0700

    new package

---
+++ src/main/java/com/m3ngsze/sentry/onlineexaminationapi/serviceCBC/impl/DetailServiceImpl.java
@@ -0,0 +1,6 @@
+package com.m3ngsze.sentry.onlineexaminationapi.serviceCBC.impl;
+
+import com.m3ngsze.sentry.onlineexaminationapi.serviceCBC.DetailService;
+
+public class DetailServiceImpl implements DetailService {
+}
