7f2ec85ce6723a3dce259f076ffbba22855e45f9
M3NGSZE <mengse.thun13@gmail.com>
2026-06-19 12:11:51 +0700

    add new folder as structure to project

--- src/main/java/com/m3ngsze/sentry/onlineexaminationapi/serviceCbc/UserCbc.java
+++
@@ -1,4 +0,0 @@
-package com.m3ngsze.sentry.onlineexaminationapi.serviceCbc;
-
-public interface UserCbc {
-}
--- src/main/java/com/m3ngsze/sentry/onlineexaminationapi/serviceCbc/impl/DetailServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.m3ngsze.sentry.onlineexaminationapi.serviceCbc.impl;
-
-import com.m3ngsze.sentry.onlineexaminationapi.serviceCbc.UserCbc;
-
-public class DetailServiceImpl implements UserCbc {
-}
